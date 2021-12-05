8759dbf52404345ce0d3ad3df5b55ec03482f21d
oscargp0201 <oscargp0201@gmail.com>
2021-12-05 21:10:58 +0000

    First commit

---
+++ sapui5/webapp/Component.js
@@ -0,0 +1,40 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "logaligroup/SAPUI5/model/Models",
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
+],
+    /**
+    * @param {typeof sap.ui.core.UIComponent} UIComponent
+    */
+    function (UIComponent, Models, ResourceModel, HelloDialog) {
+        return UIComponent.extend("logaligroup.SAPUI5.Component", {
+
+            metadata: {
+              manifest : "json"
+            },
+
+            init: function () {
+                //call the init function of the parent
+                UIComponent.prototype.init.apply(this, arguments);
+                // set data model on the view
+                this.setModel(Models.createRecipient());
+                //set i18n model on the view
+                var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
+                this.setModel(i18nModel, "i18n");
+
+                this._helloDialog = new HelloDialog(this.getRootControl());
+            } ,
+            exit: function(){
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+
+            openHelloDialog: function() {
+                this._helloDialog.open();
+            }
+
+            
+        });
+    });
---
+++ sapui5/webapp/controller/App.controller.js
@@ -0,0 +1,19 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+    * @param {typeof sap.ui.core.mvc.Controller} Controller
+    */
+    function (Controller) {
+        "use strict";
+        return Controller.extend("logaligroup.SAPUI5.controller.App", {
+            onInit: function () {
+            },
+
+            onOpenDialogHeader: function (){
+                this.getOwnerComponent().openHelloDialog();
+            }
+
+        });
+    });    
